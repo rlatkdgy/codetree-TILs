eb414729ad3b6b86c311609449bcc02c3f2fb521
rlatkdgy <103237263+rlatkdgy@users.noreply.github.com>
2024-08-12 20:26:10 +0900

    Title: ab 사이에 있는 c, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240812/ab 사이에 있는 c/c-between-a-and-b.py
+++ 240812/ab 사이에 있는 c/c-between-a-and-b.py
@@ -1,9 +1,12 @@
 a, b, c = map(int,input().split())
-
+cnt1 = 0
+cnt2 = 0
 
 while a <= b:
     if a % c ==0:
-        print("YES")
-    else :
-        print("NO")
+        cnt1 += 1
     a += 1
+if cnt1 == 0:
+    print("NO")
+else:
+    print("YES")
