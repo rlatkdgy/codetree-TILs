6e5420bdfb6da4213e7115f445a9d620f1c88a64
rlatkdgy <103237263+rlatkdgy@users.noreply.github.com>
2024-08-20 22:46:21 +0900

    Title: 체크 다이아몬드, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240820/체크 다이아몬드/check-diamond.py
+++ 240820/체크 다이아몬드/check-diamond.py
@@ -2,10 +2,20 @@ n = int(input())
 
 
 
-for i in range(n):
+for i in range(1, n+1):
     
-    for _ in range(n-i-1):
-        print(end= " ")
-    for _ in range(i-n):
-        print()
+    for _ in range(n-i):
+        print(end = " ")
+    for _ in range(i):
+        print("* ",end="")
     print()
+for i in range(1, n+1):
+    for _  in range(i):
+        print(end =" ")
+
+    for _ in range(n-i):
+        print("* ", end="")
+    print()
+
+    # for _ in range(i-1):
+    #     print("*", end=" ")
