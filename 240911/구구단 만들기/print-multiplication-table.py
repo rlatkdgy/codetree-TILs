75998c4c0c1fe3d8cbc43a5fec5e409ad9a759ca
rlatkdgy <103237263+rlatkdgy@users.noreply.github.com>
2024-09-11 17:45:51 +0900

    Title: 구구단 만들기, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240911/구구단 만들기/print-multiplication-table.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+cnt = 0
+
+for i in range(1, 10):
+
+    for j in range(b, a-1, -1):
+        if j % 2== 0:
+            print(f"{j} * {i} = {j * i}", end=" ")
+            if j > a:
+                print("/", end =" ")
+
+    print()
