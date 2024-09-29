2670c2bcc7674d3350af26ebb15e94a67b9a33ce
rlatkdgy <103237263+rlatkdgy@users.noreply.github.com>
2024-09-29 19:44:49 +0900

    Title: 완전수, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240929/완전수/perfect-number.py
@@ -0,0 +1,17 @@
+start, end = map(int, input().split())
+
+
+
+cnt = 0
+a = 0 
+while start <= end:
+
+    for i in range(1, start):
+        if cnt % i ==0:
+            a += i
+        if a == start:
+            cnt += 1
+
+
+    start += 1
+print(cnt)
