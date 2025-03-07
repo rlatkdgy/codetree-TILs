dd6df9d10446c6f401b45214cdc31edcec4b3073
rlatkdgy <103237263+rlatkdgy@users.noreply.github.com>
2025-03-07 21:41:49 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250307/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a=13;
+    double b = 0.165;
+
+    cout <<fixed << setprecision(6)<< a << " * " << b << " = " << a*b << endl;
+
+    return 0;
+}
