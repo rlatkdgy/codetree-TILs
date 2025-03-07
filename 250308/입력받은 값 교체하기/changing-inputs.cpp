a056c13b98f2601abbb0444e2849954bd897af8d
rlatkdgy <103237263+rlatkdgy@users.noreply.github.com>
2025-03-08 01:01:11 +0900

    Title: 입력받은 값 교체하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250308/입력받은 값 교체하기/changing-inputs.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a=0, b=0;
+    cin >> a;
+    cin >> b;
+    cout << b << " " << a;
+    return 0;
+}
