2e5d3700db5d711a50862fdc27b85e148694c589
rlatkdgy <103237263+rlatkdgy@users.noreply.github.com>
2025-03-07 22:05:56 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250307/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a,b,c;
+    cin >> a >> b >> c;
+
+    cout << fixed << setprecision(3)<< a << endl;
+    cout << fixed << setprecision(3)<< b << endl;
+    cout << fixed << setprecision(3)<< c << endl;
+
+
+
+    return 0;
+}
