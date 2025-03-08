18decc0a4b14185f5facb23b29dfc21933adc646
rlatkdgy <103237263+rlatkdgy@users.noreply.github.com>
2025-03-08 16:10:30 +0900

    Title: 시험 통과 여부 확인하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250308/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    int a = 80-N;
+    cin >> N;
+    
+
+    if (N>=80){
+        cout << "pass" << endl;
+    }else{
+        cout << a <<  "more score"<< endl;
+    }
+    return 0;
+}
