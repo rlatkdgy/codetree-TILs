f0c0184494c998f1eabc59ca4d603967af93a0e3
rlatkdgy <103237263+rlatkdgy@users.noreply.github.com>
2025-03-09 13:58:07 +0900

    Title: 등급 매기기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250309/등급 매기기/ranking.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+
+
+    if(N>=90){
+        cout << "A" << endl;
+    }else if(N>=80){
+        cout << "B" << endl;
+    }else if(N>=70){
+        cout << "C" << endl;
+    }else if(N>=60){
+        cout << "D" << endl;
+    }else{
+        cout << "F" << endl;
+    }
+    return 0;
+}
