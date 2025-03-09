5ac2d1966b964ef60cd09aa0679ae6077e68d4f7
rlatkdgy <103237263+rlatkdgy@users.noreply.github.com>
2025-03-09 14:33:01 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250309/점수 비교/score-comparison.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A,B, C, D;
+    cin >> A >> B;
+    cin >> C >> D;
+
+
+    cout << (A>C && B>D) <<endl;
+    
+
+
+    return 0;
+}
