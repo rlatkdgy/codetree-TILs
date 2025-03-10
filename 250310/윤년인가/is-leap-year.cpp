f448ac75fcc2dded1af6243c624eeea909433177
rlatkdgy <103237263+rlatkdgy@users.noreply.github.com>
2025-03-10 22:27:02 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250310/윤년인가/is-leap-year.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int Y;
+    cin >> Y;
+
+    if(Y%4==0){
+        if(Y%100==0 &&Y%400 !=0){
+            cout << "false" << endl;
+        }
+        cout << "true" << endl;
+    }
+    return 0;
+}
