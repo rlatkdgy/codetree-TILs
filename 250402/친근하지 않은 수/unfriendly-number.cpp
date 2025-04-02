558cca271de01f90f6554b312c822cbbc92cd825
rlatkdgy <103237263+rlatkdgy@users.noreply.github.com>
2025-04-02 23:37:57 +0900

    Title: 친근하지 않은 수, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250402/친근하지 않은 수/unfriendly-number.cpp
+++ 250402/친근하지 않은 수/unfriendly-number.cpp
@@ -2,24 +2,19 @@
 using namespace std;
 
 int main() {
-    // Please write your code here.
-    //친근한수는 2,3,5로 떨어지는수
     int N;
-    int cnt =0;
+    int cnt = 0;
     cin >> N;
 
-    for (int i=1; i<N+1; i++){
-        if(i%5==0){
-            cnt += 1;
-            break;
-        }else if(i%3==0){
-            cnt +=1;
-            break;
-        }else if(i%2==0){
-            cnt +=1;
-        }
-
+for (int i = 1; i <= N; i++){
+    if(i%5==0){
+        cnt += 1;
+    }else if(i%3==0){
+        cnt +=1;
+    }else if(i%2==0){
+        cnt +=1;
     }
-    cout << cnt ;
-    return 0;
+
+}
+cout << N-cnt ;
 }
