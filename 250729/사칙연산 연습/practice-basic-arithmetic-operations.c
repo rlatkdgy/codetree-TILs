25074d36f5f186cf8d0c04a0ff825c0187ab4bd1
rlatkdgy <103237263+rlatkdgy@users.noreply.github.com>
2025-07-29 22:19:33 +0900

    Title: 사칙연산 연습, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250729/사칙연산 연습/practice-basic-arithmetic-operations.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a = 5;
+    int b = 30;
+    int c = a+b;
+
+    int d = c/8;
+    int e = c%8;
+
+    printf("%d\n%d", d,e);
+    return 0;
+}
