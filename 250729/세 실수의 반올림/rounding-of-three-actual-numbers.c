c5e21f99a7edd51aa6fcb82d648caf3a9034e6fe
rlatkdgy <103237263+rlatkdgy@users.noreply.github.com>
2025-07-29 22:56:42 +0900

    Title: 세 실수의 반올림, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250729/세 실수의 반올림/rounding-of-three-actual-numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    double a;
+    scanf("%lf", &a);
+    double b;
+    scanf("%lf", &b);
+    double c;
+    scanf("%lf", &c);
+
+    printf("%.3lf\n%.3lf\n%.3lf", a,b,c);
+
+
+    return 0;
+}
